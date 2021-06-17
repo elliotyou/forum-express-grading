f77be019a5312efabf81a1cc1225c27ef3a94307
elliotyou <m9402311@mail.ntust.edu.tw>
2021-06-17 20:18:59 +0800

    Add admin restaurant post feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -10,6 +10,23 @@ const adminController = {
   },
   createRestaurant: (req, res) => {
     return res.render('admin/create')
+  },
+  postRestaurant: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_msg', 'name did not exist')
+      return res.redirect('back')
+    }
+    return Restaurant.create({
+      name: req.body.name,
+      tel: req.body.tel,
+      address: req.body.address,
+      opening_hours: req.body.opening_hours,
+      description: req.body.description
+    })
+      .then(restaurant => {
+        req.flash('success_msg', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
   }
 }
 
