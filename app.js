509d154a178f0d6d64a830bfeaa96c6905b78fc3
elliotyou <m9402311@mail.ntust.edu.tw>
2021-06-19 12:42:18 +0800

    add port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
