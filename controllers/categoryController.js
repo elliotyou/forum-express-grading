19107b11ce4b43f61c122d387b692f7bd9750f99
elliotyou <m9402311@mail.ntust.edu.tw>
2021-06-21 21:09:54 +0800

    Add admin categories post feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -6,6 +6,18 @@ const categoryController = {
       .then(categories => {
         return res.render('admin/categories', { categories })
       })
+  },
+  postCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_msg', 'name doesn\'t exist')
+      return res.redirect('back')
+    } else {
+      return Category.create({
+        name: req.body.name
+      }).then(() => {
+        res.redirect('/admin/categories')
+      })
+    }
   }
 }
 
