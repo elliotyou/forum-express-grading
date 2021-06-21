1adacce6377a53523929304ad21b4090e4b1076f
elliotyou <m9402311@mail.ntust.edu.tw>
2021-06-21 18:35:46 +0800

    Show category name on admin/restaurant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -59,11 +59,10 @@ const adminController = {
     }
   },
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, {
-      raw: true
-    }).then(restaurant => {
-      return res.render('admin/restaurant', { restaurant })
-    })
+    return Restaurant.findByPk(req.params.id, { include: [Category] })
+      .then(restaurant => {
+        return res.render('admin/restaurant', { restaurant: restaurant.toJSON() })
+      })
   },
   editRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, { raw: true })
