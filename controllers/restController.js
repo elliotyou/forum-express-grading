afa6287abc5910a2922bb08dfd843f28c288f39b
elliotyou <m9402311@mail.ntust.edu.tw>
2021-06-22 15:40:29 +0800

    Show related comments on restaurant page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const Comment = db.Comment
+const User = db.User
 
 const pageLimit = 10
 
@@ -42,7 +44,12 @@ const restController = {
     }
   },
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { include: Category })
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: Comment, include: [User] }
+      ]
+    })
       .then(restaurant => {
         res.render('restaurant', { restaurant: restaurant.toJSON() })
       })
