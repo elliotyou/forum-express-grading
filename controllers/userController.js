84da071d740f61b5efa6e9a62e080c25940d958a
elliotyou <m9402311@mail.ntust.edu.tw>
2021-06-23 21:24:14 +0800

    modify parameter name 'user' to 'targedUser' to avoid ambiguity

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,9 +64,9 @@ const userController = {
       })
       const restaurants = comments.map(comment => comment.Restaurant)
       const count = restaurants.length
-      const user = await User.findByPk(req.params.id)
+      const targedUser = await User.findByPk(req.params.id)
       return res.render('user', {
-        user: user.toJSON(),
+        targedUser: targedUser.toJSON(),
         restaurants,
         count,
         isMySelf
